5d603a096e1039145edd8a37fcf397e46a670757
Basile <interbeing34@gmail.com>
2022-03-20 22:33:54 +0300

    new branch

--- src/main/java/helpers/TestingConfiguration.java
+++ src/main/java/helpers/TestingConfiguration.java
@@ -1,5 +1,7 @@
 package helpers;
 
+import java.util.Objects;
+
 public class TestingConfiguration {
 
     private TestingConfiguration() {
@@ -8,4 +10,22 @@ public class TestingConfiguration {
     public static String getStartUrl() {
         return TestingEnvironment.getCurrentEnvironment("testing.json").getValue("/start_url").toString();
     }
+
+    public static String setStartBrowser(){
+        return TestingEnvironment.getCurrentEnvironment("settings.json").getValue("/browserName").toString();
+    }
+
+    public static void main(String[] args) {
+        String value = setStartBrowser();
+        System.out.println(value);
+        if (value.equals("chrome")) {
+            // if (Objects.equals(setStartBrowser(), "${BROWSER}"))
+            value = "edge";
+            System.out.println(value);
+        }
+
+
+
+    }
+
 }
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,18 +2,31 @@ import aquality.selenium.browser.AqualityServices;
 import aquality.selenium.browser.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import static helpers.TestingConfiguration.*;
+import java.util.Objects;
 
 public abstract class BaseTest {
-    protected Browser browser;
+    protected Browser browser = AqualityServices.getBrowser();;
+
 
     @BeforeMethod
     public void setUp() {
-        browser = AqualityServices.getBrowser();
+       /* String value = setStartBrowser();
+        System.out.println(value + "  qweqweqwe");
+        if (value.equals("chrome")) {
+            // if (Objects.equals(setStartBrowser(), "${BROWSER}"))
+            value = "edge";
+            System.out.println(value + "11111111");
+        }*/
+
+      //  browser = AqualityServices.getBrowser();
         browser.maximize();
     }
+        @AfterMethod
+        public void tearDown () {
+            browser.quit();
+        }
 
-    @AfterMethod
-    public void tearDown() {
-        browser.quit();
-    }
 }
