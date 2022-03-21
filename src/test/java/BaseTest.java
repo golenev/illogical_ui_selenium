526a34820ea9c71f217e5bd2bcd772db66f42c58
Basile <interbeing34@gmail.com>
2022-03-21 17:25:49 +0300

    refactoring

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,31 +2,19 @@ import aquality.selenium.browser.AqualityServices;
 import aquality.selenium.browser.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-
-import static helpers.TestingConfiguration.*;
-import java.util.Objects;
 
 public abstract class BaseTest {
-    protected Browser browser = AqualityServices.getBrowser();;
-
+    protected Browser browser = AqualityServices.getBrowser();
 
     @BeforeMethod
     public void setUp() {
-       /* String value = setStartBrowser();
-        System.out.println(value + "  qweqweqwe");
-        if (value.equals("chrome")) {
-            // if (Objects.equals(setStartBrowser(), "${BROWSER}"))
-            value = "edge";
-            System.out.println(value + "11111111");
-        }*/
 
-      //  browser = AqualityServices.getBrowser();
         browser.maximize();
     }
-        @AfterMethod
-        public void tearDown () {
-            browser.quit();
-        }
+
+    @AfterMethod
+    public void tearDown() {
+        browser.quit();
+    }
 
 }
